d50d688b29b19fcf51e13e1102df9d97862808ad
Yizhou <yizhou_zhao@berkeley.edu>
2022-05-27 18:04:45 -0700

    add conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,14 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'VRKitchen2.0'
+copyright = '2022, UCLA'
+author = 'Yizhou Zhao, Wensi Ai'
 
-release = '0.1'
-version = '0.1.0'
+# The short X.Y version.
+version = "0.4"
+# The full version, including alpha/beta/rc tags.
+release = "0.0.4"
 
 # -- General configuration
 
@@ -17,6 +19,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosectionlabel',
+    'sphinxcontrib.bibtex',
+    'nbsphinx',
 ]
 
 intersphinx_mapping = {
@@ -30,6 +36,20 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "../images/logo.png"
+html_theme_options = {
+    'logo_only': True
+}
+
+html_static_path = ['_static']
+
+html_css_files = ["theme.css"]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'genmotion')))
+
+autodoc_mock_imports = ["bpy", "numpy", "torch", "matplotlib", "IPython", "pickle", "genmotion"]
