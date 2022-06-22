ad66d933c941eda304661b8e1d2634217ae53115
yizhouzhao <yizhou_zhao@berkeley.edu>
2022-06-21 17:21:49 -0700

    init isaac ext

---
+++ vrkitchen2.0/human.as.articulation/exts/human.as.articulation/human/as/articulation/extension.py
@@ -0,0 +1,22 @@
+import omni.ext
+import omni.ui as ui
+
+# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
+# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
+# on_shutdown() is called.
+class MyExtension(omni.ext.IExt):
+    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
+    # this extension is located on filesystem.
+    def on_startup(self, ext_id):
+        print("[human.as.articulation] MyExtension startup")
+
+        self._window = ui.Window("My Window", width=300, height=300)
+        with self._window.frame:
+            with ui.VStack():
+                ui.Button("Capsu test", clicked_fn=self.test_capsule)
+
+    def on_shutdown(self):
+        print("[human.as.articulation] MyExtension shutdown")
+
+    def test_capsule(self):
+        print("test_capsule")
